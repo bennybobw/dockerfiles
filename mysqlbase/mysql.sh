ec842c608d2a946318feab173b30dd4a851d2e96
Benjamin Wheeler <bwheeler@giantrabbit.com>
2014-04-23 10:55:59 -0700

    Added mysqlbase

---
+++ mysqlbase/mysql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+exec 2>&1
+if [ ! -d "/var/lib/mysql/performance_schema" ]; then
+  if [ -d "/var/lib/mysql" ]; then
+    chown -R mysql:mysql /var/lib/mysql
+    mysql_install_db
+  fi
+fi
+
+#from http://smarden.org/runit1/runscripts.html#mysql
+cd /
+umask 077
+
+MYSQLADMIN='/usr/bin/mysqladmin --defaults-extra-file=/etc/mysql/debian.cnf'
+
+trap "$MYSQLADMIN shutdown" 0
+trap 'exit 2' 1 2 3 15
+
+/usr/bin/mysqld_safe & wait
